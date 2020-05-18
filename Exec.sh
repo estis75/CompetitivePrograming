fa2ae4b4d97015e04dd310a404e2ecf273d12d44
estis75 <ktakashima1213@gmail.com>
2020-05-18 11:54:20 +0900

    update Execution to be able to output a merged file

--- Exec.sh
+++ Exec.sh
@@ -8,7 +8,9 @@ if [ $? -ne 0 ]; then
   exit 1;
 fi
 for i in `ls TestCase/Testcase*.txt`; do
+  echo "input:"
   cat $i
+  echo -e "output:\n"
   ./a.out < $i
   echo -e "\n"
 done
@@ -16,9 +18,12 @@ done
 cp `basename \`pwd\``.cpp temp.cpp
 
 touch output.cpp
-for i in `ls Templates/`; do
-  cat Templates/$i | grep -v "include|define">> output.cpp
+cat Templates/libraries.h | head -2 >> output.cpp
+for i in `cat Templates/libraries.h | grep "include\".*\""`; do
+  i=`echo ${i/\#include/} | sed -e 's/\"//g' `
+  cat Templates/$i | sed -e '/using/d' | sed -e '/include/d' >> output.cpp
 done
+sed -e '/\#include\".*\"/d' Templates/libraries.h | tail -n +3 >> output.cpp
 sed '1,3d' temp.cpp >> output.cpp
 
 rm temp.cpp
